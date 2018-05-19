1bc38421c6620d7311f44fb3a2b1a2a108a1d7b6
rzhedunov <rzhedunov@mail.ru>
2018-05-19 10:42:07 +0300

    Добавьте файлы проекта.

---
+++ Putilin2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Putilin2018.Startup))]
+namespace Putilin2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
