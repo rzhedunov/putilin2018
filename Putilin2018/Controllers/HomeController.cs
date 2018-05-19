2eaf8b59b52f57223c8120be6a3e77629c2308b2
rzhedunov <rzhedunov@mail.ru>
2018-05-19 12:12:47 +0300

    Первый коммит

--- Putilin2018/Controllers/HomeController.cs
+++ Putilin2018/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Putilin2018.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Страница описания моего приложения";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            //ViewBag.Message = "Мои контакты";
 
             return View();
         }
