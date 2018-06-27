724dd88536c27e5361b49c984ce8b4b73a1ce3a9
R20171220N\R20171220 <jake_r7@mail.ru>
2018-06-27 18:48:03 +0300

    Дописал отчет о расходе ГСМ

--- Putilin2018/Controllers/ViewРасходГСМController.cs
+++ Putilin2018/Controllers/ViewРасходГСМController.cs
@@ -14,13 +14,42 @@ namespace Putilin2018.Controllers
     {
         private MyDatabaseEntities db = new MyDatabaseEntities();
 
+        [HttpPost]
         // GET: ViewРасходГСМ
-        public ActionResult Index()
-        {
-            return View(db.ViewРасходГСМ.ToList());
+        public ActionResult Index(FormCollection collection)
+        {            
+            DateTime date_start = (collection["date_start"]==null) ? DateTime.Now : Convert.ToDateTime(collection["date_start"]);
+            DateTime date_end = (collection["date_end"] == null) ? DateTime.Now : Convert.ToDateTime(collection["date_end"]);
+
+            ViewBag.date_start = "" + (date_start.Day < 10 ? "0" : "") + date_start.Day + "." + (date_start.Month < 10 ? "0" : "") + date_start.Month + "." + date_start.Year;
+            ViewBag.date_end = "" + (date_end.Day < 10 ? "0" : "") + date_end.Day + "." + (date_end.Month < 10 ? "0" : "") + date_end.Month + "." + date_end.Year;
+
+
+            var reportdata = db.ViewРасходГСМ.Where(x => x.Дата_рейса> date_start && x.Дата_рейса < date_end);
+
+            reportdata = reportdata.OrderBy(x => x.Дата_рейса);
+
+            float sum = 0;
+            foreach (ViewРасходГСМ x in reportdata) sum += x.Расход_ГСМ;
+            ViewBag.sum = sum;
+
+            return View(reportdata.ToList());
         }
 
-       
+        public ActionResult ChoosePeriod()
+        {
+            DateTime date_start = DateTime.Now.AddMonths(-1) ;
+            DateTime date_end = DateTime.Now;
+
+            ViewBag.date_start = "" + date_start.Year + "-" + (date_start.Month < 10 ? "0" : "") + date_start.Month + "-" + (date_start.Day < 10 ? "0" : "") + date_start.Day;
+            ViewBag.date_end = "" + date_end.Year + "-" + (date_end.Month < 10 ? "0" : "") + date_end.Month + "-" + (date_end.Day < 10 ? "0" : "") + date_end.Day;
+
+
+            return View();
+
+        }
+
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
---
+++ Putilin2018/Models/DataPeriod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Putilin2018.Models
+{
+    public class DataPeriod
+    {
+        public DateTime date_start { get; set; }
+        public DateTime date_end { get; set; }
+    }
+}
